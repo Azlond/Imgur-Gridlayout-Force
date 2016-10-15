4f0f55a5b6cf69086800f9af34960dcb7e3cc12e
Jan <github@sintho.com>
2016-10-15 21:01:33 +0200

    bug fix if url ends with "/"
    fixed a bug where the redirect would fail if the URL ended with a slash
    "/"
    
    changed version number
    
    changed contentID to galleryID for better explanation of the variable

--- GridForcer.user.js
+++ GridForcer.user.js
@@ -5,15 +5,20 @@
 // @description Forces gridlayout on imgur
 // @include     https://imgur.com/gallery/*
 // @include     https://imgur.com/a/*
-// @version     1.0
+// @version     1.1
 // @grant       none
 // ==/UserScript==
 
-var currentLink = window.location + ""; //window.location contrains the link of the currently visited website
-var regex = new RegExp("grid", "i"); // Regular expression to see if "grid" is in the URL
+var currentLink = window.location + ""; //window.location contains the link of the currently visited website
+var regex = new RegExp("grid", "i"); // Regular expression checking for "grid" 
+var galleryID; //ID of the gallery
 
 if (!regex.test(currentLink)) { // if there is no "grid" in the current url
 	var res = currentLink.split("/"); // Split the url into an array of substrings by using the separator "/"
-	var contentID = res[res.length - 1]; // get the last element of the array, which is the ID of the gallery
-	window.location = "https://imgur.com/a/" + contentID + "?grid"; // redirect to the gallery, but add "?grid" to the url, thereby forcing the grid layout
+	if (res[res.length - 1].length < 1) { //if there's a "/" as last character, we need to get the second to last element from the array
+		galleryID= res[res.length - 2]; // get the second to last element of the array, which is the ID of the gallery
+	} else { // else: the url ends with the galleryID
+		galleryID = res[res.length-1]; // get the last element of the array, which is the ID of the gallery
+	}
+	window.location = "https://imgur.com/a/" + galleryID + "?grid"; // redirect to the gallery, but add "?grid" to the url, thereby forcing the grid layout
 }
